3176cfe7773ed2a64e513acf49f766f29ec072c4
Rakesh202010 <60429332+Rakesh202010@users.noreply.github.com>
2024-01-31 12:47:29 +0530

    removed commented

--- config/database.py
+++ config/database.py
@@ -5,16 +5,6 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-# Database details
-# DB_HOST = '34.100.182.69'
-# DB_PORT = '3306'
-# DB_NAME = 'cloudnativedb'
-# DB_USER = 'your_database_user'
-# DB_PASSWORD = 'your_database_password'
-# CLOUD_SQL_CONNECTION_NAME = 'gcp-data-project-410116:asia-south1:cloudnativedb-instance1'
-
-# connection_url = f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
 # Database configuration settings
 DATABASE_URL = "mysql+mysqlconnector://root:@localhost:3306/medical"
 
